6fd8bb77a33146d22c803c76988ab8fbf9f5c12e
Cristian Henrique <crissshenri180467@gmail.com>
2023-08-27 05:59:28 -0300

    Update resolutions

--- exercises-functions/exercise-bmi/scripts.js
+++ exercises-functions/exercise-bmi/scripts.js
@@ -3,8 +3,8 @@ const prompt = require('prompt-sync')()
 const weight = prompt('Digite o peso:')
 const height = prompt('Digite a altura:')
 
-function imc(weight, height) {
+function bmi(weight, height) {
     let imc = weight / Math.pow(height, 2)
     return console.log(`IMC = ${imc.toFixed(2)}`)
 }
-imc(weight, height)
+bmi(weight, height)
--- exercises-functions/exercise-temperature/scripts.js
+++ exercises-functions/exercise-temperature/scripts.js
@@ -2,8 +2,8 @@ const prompt = require('prompt-sync')()
 
 const fahrenheit = prompt('Digite a temperatura em Fahrenheit:')
 
-function fahrenheitToCelsius(fahrenheit) {
-    let celsius = 5/9 * (fahrenheit - 32)
+function fahrenheitToCelsius(f) {
+    let celsius = 5/9 * (f - 32)
     return console.log(`Temperatura em Celsius = ${celsius.toFixed(2)}`)
 }
 fahrenheitToCelsius(fahrenheit)
